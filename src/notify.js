9c8ddaae30014214ddbf4a3d8fc5ec0adaff3707
cho45 <cho45@lowreal.net>
2011-03-30 21:54:31 +0900

    Bug fixes

--- src/notify.js
+++ src/notify.js
@@ -6,10 +6,10 @@ Hatena.Login = {};
 Hatena.Login.API = {
     identify : function () {
         return next(function () {
-            return http.get('http://n.hatena.ne.jp/applications/my.json').
+            return http.get('http://n.hatena.ne.jp/applications/my.json?t=' + new Date().getTime()).
             next(function (r) {
                 var data = JSON.parse(r.responseText);
-                return data.url_name;
+                return data.url_name || null;
             }).
             error(function () {
                 return null;
@@ -25,6 +25,22 @@ Hatena.Login.API = {
             domain : ".hatena.ne.jp",
             path : "/",
             expirationDate : new Date().getTime() + 60 * 60 * 24 * 365
+        });
+
+        var self = this;
+        return next(function check () {
+            return self.getRk().next(function (val) {
+                if (val == rk) {
+                    return true;
+                } else {
+                    webkitNotifications.createNotification(
+                        '48.png',
+                        'Hatena',
+                        'failed to set rk'
+                    ).show();
+                    return wait(0.2).next(check);
+                }
+            });
         });
     },
 
@@ -72,6 +88,17 @@ Hatena.Login.Setting = {
             chrome.browserAction.setBadgeBackgroundColor({ color: [100, 100, 100, 200] });
             chrome.browserAction.setBadgeText({ text : val || '' });
             chrome.browserAction.setTitle({ title : val || '' });
+            chrome.browserAction.setIcon({ path : '16.png' });
+            var img = new Image();
+            img.onload = function () {
+                var canvas = document.createElement('canvas');
+                canvas.width = 19;
+                canvas.height = 19;
+                var ctx = canvas.getContext('2d');
+                ctx.drawImage(img, 0, 0, 18, 18);
+                chrome.browserAction.setIcon({ imageData : ctx.getImageData(0, 0, 18, 18) });
+            };
+            img.src = 'http://www.st-hatena.com/users/' + val.substring(0, 2) + '/' + val + '/profile.gif';
             localStorage['current'] = val;
         }
         return localStorage['current'];
@@ -126,6 +153,8 @@ Hatena.Login['popup'] = function () {
     var parent = document.getElementById('list');
     parent.innerHTML = '';
 
+    Hatena.Login.Setting.del('undefined');
+
     var current = Hatena.Login.Setting.current();
     var users = Hatena.Login.Setting.get();
     for (var key in users) if (users.hasOwnProperty(key)) {
@@ -143,33 +172,35 @@ Hatena.Login['popup'] = function () {
             var user = e.target._user;
             var rk   = e.target._rk;
             Hatena.Login.Setting.current(user);
-            Hatena.Login.API.setRk(rk);
-            Hatena.Login.API.identify().
-            next(function (iuser) {
-                if (iuser == user) {
-                    chrome.tabs.getSelected(undefined, function (tab) {
-                        chrome.tabs.executeScript(tab.id, { code : 'location.reload(true)' }, function () {});
-                        window.close();
-                    });
-                } else {
-                    Hatena.Login.API.getRk().
-                    next(function (cookie) {
-                        if (cookie) {
-                            window.open('http://www.hatena.ne.jp/logout?location=' + encodeURIComponent('http://www.hatena.ne.jp/login'));
-                        } else {
-                            window.open('http://www.hatena.ne.jp/login');
-                        }
-                    });
-                    var notification = webkitNotifications.createNotification(
-                        '48.png',
-                        'Hatena',
-                        'クッキーが切れたようなのでもっかいマミさんもぐもぐしてください'
-                    );
-                    notification.show();
-                    setTimeout(function () {
-                        notification.cancel();
-                    }, 3000);
-                }
+            Hatena.Login.API.setRk(rk).
+            next(function () {
+                Hatena.Login.API.identify().
+                next(function (iuser) {
+                    if (iuser == user) {
+                        chrome.tabs.getSelected(undefined, function (tab) {
+                            chrome.tabs.executeScript(tab.id, { code : 'location.reload(true)' }, function () {});
+                            window.close();
+                        });
+                    } else {
+                        Hatena.Login.API.getRk().
+                        next(function (cookie) {
+                            if (cookie) {
+                                window.open('http://www.hatena.ne.jp/logout?location=' + encodeURIComponent('http://www.hatena.ne.jp/login'));
+                            } else {
+                                window.open('http://www.hatena.ne.jp/login');
+                            }
+                        });
+                        var notification = webkitNotifications.createNotification(
+                            '48.png',
+                            'Hatena',
+                            'クッキーがきれたので再ログインしてください。'
+                        );
+                        notification.show();
+                        setTimeout(function () {
+                            notification.cancel();
+                        }, 3000);
+                    }
+                });
             });
         }, false);
         parent.appendChild(li);
